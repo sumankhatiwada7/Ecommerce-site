bfe8b86c1e6135fed98b912ff6556d29ee109ecf
suman khatiwada Github <sumankhatiwada786@gmail.com>
2025-08-12 15:38:11 +0545

    catgory  section is completed

--- resources/views/admin/category/manage.blade.php
+++ resources/views/admin/category/manage.blade.php
@@ -8,5 +8,33 @@ Manage Categories - Admin Panel
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
         {{ __(' Manage categories') }}
     </h2>
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Categories</h3>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->category_name }}</td>
+                        <td>
+                         <a href="#" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
 
 @endsection
